1d4a6938ca062d2068070753e109396e2ea19233
Juuxel <kasperi.kauppi@gmail.com>
2015-09-22 19:57:42 +0300

    Fixed mcmod.info

--- src/main/java/juuxel/stevesworkfix/reference/Reference.java
+++ src/main/java/juuxel/stevesworkfix/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
     public static final String MOD_ID = "StevesWorkfix";
     public static final String NAME = "Steve's Workfix";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String DEPENDENCIES = "required-after:StevesWorkshop";
 }
